e715edd9044ebd6f6589713a1a61f93a15719a6a
NikolasEagle <eagle.dev.stack@gmail.com>
2024-10-28 19:46:45 +0400

    Добавление

--- app.js
+++ app.js
@@ -6,17 +6,45 @@ import path from "path";
 
 import fs from "fs";
 
-let httpsOptions = {
+const port = 5000;
+
+/*const httpsOptions = {
   key: fs.readFileSync("privkey.pem"),
 
   cert: fs.readFileSync("fullchain.pem"),
-};
+};*/
 
 let app = express();
 
-app.get("/", async (req, res) => {
-  res.sendFile(path.resolve("public/index.html"));
+app.get("/api/films/:page", async (req, res) => {
+  try {
+    let response = await fetch(
+      `https://kinobd.xyz/api/films?page=${req.params.page}`
+    );
+
+    let body = await response.json();
+
+    res.send(body);
+  } catch (error) {
+    res.send({});
+  }
 });
+
+app.get("/api/films/search/:q", async (req, res) => {
+  try {
+    let response = await fetch(
+      `https://kinobd.xyz/api/films/search/title?q=${req.params.q}`
+    );
+
+    let body = await response.json();
+
+    res.send(body);
+  } catch (error) {
+    res.send({});
+  }
+});
+
+app.listen(port, () => console.log(`Server is running on ${port}`));
 
 export { app };
 
