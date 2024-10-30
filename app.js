fbdb87add3185402aaf63d8b65e21d9916b343b7
NikolasEagle <eagle.dev.stack@gmail.com>
2024-10-30 16:17:00 +0400

    Редактирование gitignore

--- app.js
+++ app.js
@@ -17,8 +17,26 @@ const httpsOptions = {
 let app = express();
 
 app.get("/", async (req, res) => {
+  res.redirect("/pages/1");
+});
+
+app.get("/pages/:page", async (req, res) => {
   res.sendFile(path.resolve("public/index.html"));
 });
+
+app.get("/search/:query/pages/:page", async (req, res) => {
+  res.sendFile(path.resolve("public/index.html"));
+});
+
+app.get("/search/:query/pages/:page", async (req, res) => {
+  res.sendFile(path.resolve("public/index.html"));
+});
+
+app.use(express.static(path.resolve("public")));
+
+/*app.listen(3000, () =>
+  console.log("Testing server is running on http://localhost:8000")
+);*/
 
 https
   .createServer(httpsOptions, app)
